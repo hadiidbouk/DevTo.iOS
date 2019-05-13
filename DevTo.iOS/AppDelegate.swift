fa9bbece4b493bd0f030c503d0b8586203dfaeec
Hadi Dbouk <hadiidbouk@outlook.com>
2019-05-13 14:11:34 +0300

    Initial Commit

---
+++ DevTo.iOS/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  DevTo.iOS
+//
+//  Created by Hadi Dbouk on 5/13/19.
+//  Copyright © 2019 hadiidbouk. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
