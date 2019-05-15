80a277dc91c73d513a330778dadcf987af8aa630
Hadi Dbouk <hadiidbouk@outlook.com>
2019-05-15 13:38:39 +0300

    Add FeedType enum

---
+++ DevTo.iOS/Scenarios/Feed/Models/FeedType.swift
@@ -0,0 +1,19 @@
+//
+//  FeedType.swift
+//  DevTo.iOS
+//
+//  Created by Hadi Dbouk on 5/15/19.
+//  Copyright © 2019 hadiidbouk. All rights reserved.
+//
+
+import Foundation
+
+enum FeedType : String {
+    
+    case myDevFeed = "<MY DEV FEED>"
+    case pastWeek = "<PAST WEEK>"
+    case pastMonth = "<PAST MONTH>"
+    case pastYear = "<PAST YEAR>"
+    case infinity = "<INFINITY>"
+    case latest = "<LATEST>"
+}
