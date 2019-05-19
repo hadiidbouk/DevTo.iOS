0f32ca7a777ce3aed132562bca3341c14a90f4dd
Hadi Dbouk <hadiidbouk@outlook.com>
2019-05-19 17:39:28 +0300

    Add pop and push animation

---
+++ DevTo.iOS/Misc/Extensions/UINavigationController+Extensions.swift
@@ -0,0 +1,56 @@
+//
+//  UINavigationController+Extensions.swift
+//  DevTo.iOS
+//
+//  Created by Hadi Dbouk on 5/19/19.
+//  Copyright © 2019 hadiidbouk. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    
+    func popToViewController(viewController: UIViewController) {
+        
+        for vc in viewControllers {
+            
+            pop()
+
+            if vc == viewController {
+                return
+            }
+        }
+    }
+    
+    /**
+     Pop current view controller to previous view controller.
+     
+     - parameter type:     transition animation type.
+     - parameter duration: transition animation duration.
+     */
+    func pop(transitionType type: CATransitionType = .fade, duration: CFTimeInterval = 0.3) {
+        self.addTransition(transitionType: type, duration: duration)
+        self.popViewController(animated: false)
+    }
+    
+    /**
+     Push a new view controller on the view controllers's stack.
+     
+     - parameter vc:       view controller to push.
+     - parameter type:     transition animation type.
+     - parameter duration: transition animation duration.
+     */
+    func push(viewController vc: UIViewController, transitionType type: CATransitionType = .fade, duration: CFTimeInterval = 0.3) {
+        self.addTransition(transitionType: type, duration: duration)
+        self.pushViewController(vc, animated: false)
+    }
+    
+    private func addTransition(transitionType type: CATransitionType = .fade, duration: CFTimeInterval = 0.3) {
+        let transition = CATransition()
+        transition.duration = duration
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        transition.type = type
+        self.view.layer.add(transition, forKey: nil)
+    }
+    
+}
