daba475ee42efe3fef1657a1214ade05c84e54d1
Hadi Dbouk <hadiidbouk@outlook.com>
2019-05-19 17:41:40 +0300

    Go to FeedViewController when tapping dev Image

--- DevTo.iOS/Scenarios/BaseViewController.swift
+++ DevTo.iOS/Scenarios/BaseViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class BaseViewController : UIViewController {
+    
+    private let disposeBag = DisposeBag()
     
     var navigationBarView: NavigationBarView!
 
     func setupNavigationBarView() {
         navigationBarView = NavigationBarView()
         navigationBarView.setToTop(view: view)
+        
+        navigationBarView.devTapGesture.rx.event.bind(onNext: { [weak self] recognizer in
+            if let viewControllers = self?.navigationController?.viewControllers {
+                self?.navigationController?.popToViewController(viewController: viewControllers[0])
+            }
+        }).disposed(by: disposeBag)
     }
 }
