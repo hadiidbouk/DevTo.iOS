587c8c112afe86c090df9d3ed5dd04e340cb9b6c
Hadi Dbouk <hadiidbouk@outlook.com>
2019-05-20 15:14:36 +0300

    Add Testing CoverImage to scrollable stackview in ArticleDetailsViewController

--- DevTo.iOS/Scenarios/ArticleDetails/ArticleDetailsViewController.swift
+++ DevTo.iOS/Scenarios/ArticleDetails/ArticleDetailsViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ArticleDetailsViewController: BaseViewController {
 
     var bottomView: ArticleDetailsBottomView!
+    var rootStackView: UIStackView!
+    var coverImageView: UIImageView!
+    var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,10 +32,58 @@ extension ArticleDetailsViewController {
         
         setupNavigationBarView()
         setupBottomView()
+        setupRootStackView()
     }
     
     private func setupBottomView() {
         bottomView = ArticleDetailsBottomView()
         bottomView.setToBottom(view: view)
+    }
+    
+    private func setupRootStackView() {
+        
+        scrollView = UIScrollView()
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        
+        view.addSubview(scrollView)
+        scrollView.apply {
+            $0.topConstraint(onBottomOf: navigationBarView, constant: 1)
+            $0.bottomConstraint(onTopOf: bottomView, constant: -1)
+            $0.leadingConstraint(constant: 0)
+            $0.trailingConstaint(constant: 0)
+        }
+        
+        rootStackView = UIStackView()
+        rootStackView.axis = .vertical
+        rootStackView.distribution = .fill
+        rootStackView.spacing = 5
+        
+        scrollView.addSubview(rootStackView)
+        rootStackView.apply {
+            $0.stickToParentEdges()
+            $0.widthConstraint(sameAs: scrollView)
+        }
+        
+        setupCoverImageView()
+    }
+    
+    private func setupCoverImageView() {
+        
+        let coverImageViewContainer = UIView()
+        coverImageView = UIImageView()
+        coverImageView.contentMode = .scaleToFill
+        coverImageView.kf.setImage(with: URL(string: "https://res.cloudinary.com/practicaldev/image/fetch/s--wd9i1HhS--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/http://i.imgur.com/LNs7k41.png")!)
+        
+        coverImageViewContainer.addSubview(coverImageView)
+        coverImageView.apply {
+            $0.stickToParentEdges()
+        }
+        
+        rootStackView.addArrangedSubview(coverImageViewContainer)
+        
+        coverImageViewContainer.apply {
+            $0.aspectRatioConstraint(multiplier: 9/21)
+        }
     }
 }
